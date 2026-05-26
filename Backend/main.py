21ece78779f05bd5097f4d3bcef06d4fa90c1097
Burjwok Paulo Deng Ajang <burypaulo360@gmail.com>
2026-05-26 00:01:14 +0000

    Update CORS settings to allow all origins

--- Backend/main.py
+++ Backend/main.py
@@ -20,8 +20,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://yourfrontend.vercel.app"
-        "http://localhost:5173"
+        "*"
     ],
     allow_credentials=True,
     allow_methods=["*"],
